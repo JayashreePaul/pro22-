ca66cb18614bc3826e6b88559820c6711a5c3d1e
JayashreePaul <86361507+JayashreePaul@users.noreply.github.com>
2021-09-14 19:30:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,29 +15,49 @@ function preload() {
   playerArcherimage = loadImage("./assets/playerArcher.png")
 }
 
+
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-
-  engine = Engine.create();
-  world = engine.world;
-  angleMode(DEGREES);
-  angle = 30;
-
-  var options = {
-
-    isStatic: true
-  };
-
-  //create player base body
- playerBase = Bodies.rectangle(200,350,180,150,options);
- World.add(world, playerBase);
-  //create player body
- player = Bodies. rectangle(250,playerBase.position.y-160,50,180,options);
- World.add(world,player)
- 
- playerArcher = Bodies.rectangle(262,230,50,80,options,angle);
-
-}
+    canvas = createCanvas(windowWidth, windowHeight);
+  
+    engine = Engine.create();
+    world = engine.world;
+  
+    angleMode(DEGREES);
+  
+    var options = {
+      isStatic: true
+    };
+  
+    playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+    World.add(world, playerBase);
+  
+    player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+    World.add(world,player)
+  
+    playerArcher = new PlayerArcher(
+      340,
+      playerBase.position.y - 112,
+      120,
+      120
+    );
+  }
+  
+  function draw() {
+    background(backgroundImg);
+  
+    Engine.update(engine);
+    image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+    image(playerimage,player.position.x,player.position.y,50,180)
+  
+    playerArcher.display();
+  
+    // Title
+    fill("#FFFF");
+    textAlign("center");
+    textSize(40);
+    text("EPIC ARCHERY", width / 2, 100);
+  }
+  
 
 function draw() {
   background(backgroundImg);
@@ -49,8 +69,14 @@ image(playerimage,player.position.x,player.position.y,50,180)
 
 image(playerArcherimage,playerArcher.position.x,playerArcher.position.y,50,80)
  
+if (keyIsDown(RIGHT_ARROW) &&  playerArcher.angle<70  ) {
+  playerArcher.angle += 1;
+}
 
-  // Title
+if (keyIsDown(LEFT_ARROW) &&  playerArcher.angle>-30 ) {
+  playerArcher.angle -= 1;}
+
+ 
   fill("#FFFF");
   textAlign("center");
   textSize(40);
